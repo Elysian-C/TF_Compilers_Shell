d3cfa21c0af2e4e79b6819c859dde35859f6b23b
SamuelCano03 <102400537+SamuelCano03@users.noreply.github.com>
2023-11-25 11:53:47 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/zsh
+export CLASSPATH=.:~/.m2/repository/org/antlr/antlr4/4.13.1/antlr4-4.13.1-complete.jar:$CLASSPATH
+
+alias grun='java org.antlr.v4.gui.TestRig'
+
+export ANTLR4I=/home/samuel/MyApps/antlr4-cpp/run/usr/local/include
+export ANTLR4L=/home/samuel/MyApps/antlr4-cpp/build/runtime
+
